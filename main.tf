b7ed38dbb20b159b4f0c92495060abf3fe13c0d3
cyrilmoses <cyril.moses@milwaukeetool.com>
2024-08-27 04:07:38 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+  }
+}
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = {
+      STAGE              = var.stage
+    }
+  }
+}
+
+resource "aws_sns_topic" "user_updates" {
+  name = "user-updates-topic"
+}
